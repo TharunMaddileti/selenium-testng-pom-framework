621845c66b2c6b8e5f94fc786804170c21c6c698
TharunMaddileti <tharunmaddileti@gmail.com>
2026-09-05 23:44:45 +0530

    Add LoginPage class for user authentication

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,39 @@
+package com.qa.pages;
+
+import java.time.Duration;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LoginPage {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    // Locators
+    private By usernameField = By.id("username");
+    private By passwordField = By.id("password");
+    private By loginButton = By.cssSelector("button[type='submit']");
+    private By flashMessage = By.id("flash");
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    public void enterUsername(String user) {
+        wait.until(ExpectedConditions.visibilityOfElementLocated(usernameField)).sendKeys(user);
+    }
+
+    public void enterPassword(String pass) {
+        driver.findElement(passwordField).sendKeys(pass);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public String getFlashMessage() {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(flashMessage)).getText();
+    }
+}
